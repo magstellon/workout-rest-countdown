142c4861d1e185de208ce39705b7994cfc2e5b44
Nadarajah Mag-Stellon <magstellon@gmail.com>
2019-01-16 16:31:05 +0100

    Clean unuse stop feature

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faPlay, faPause, faStop, faUndo } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faPause, faUndo } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faPlay)
 library.add(faPause)
-library.add(faStop)
 library.add(faUndo)
 Vue.component('icon', FontAwesomeIcon)
 
