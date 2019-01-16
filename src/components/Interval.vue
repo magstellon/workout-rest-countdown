77c9622b97c3c55dd37f33b15df4a58eaf841e5c
Nadarajah Mag-Stellon <magstellon@gmail.com>
2019-01-16 13:58:51 +0100

    Refractor to use reactivity and more explicit naming

--- src/components/Interval.vue
+++ src/components/Interval.vue
@@ -3,13 +3,13 @@
         <svg width="300" height="300">
             <text x="46" y="165" fill="white">{{time()}}</text>
             <circle class="countdown-base" cx="150" cy="150" r="140"/>
-            <circle class="countdown" cx="150" cy="150" r="140" transform="rotate(270, 150, 150)" :stroke-dasharray="`2 ${timeLeftPercent + 2}`"/>
+            <circle class="countdown" cx="150" cy="150" r="140" transform="rotate(270, 150, 150)" :stroke-dasharray="`2 ${Math.max(timeLeft * 100 / duration, 2)}`"/>
         </svg>
 
         <icon icon="stop" class="button right" @click="$emit('stop')" />
-        <icon icon="pause" class="button left" v-if="runnable" @click="pause" />
-        <icon icon="play" class="button left" v-if="!runnable && !finish" @click="play" />
-        <icon icon="undo" class="button right" @click="refresh" />
+        <icon icon="pause" class="button left" v-if="isRunning" @click="pause" />
+        <icon icon="play" class="button left" v-if="!isRunning && !finish" @click="play" />
+        <icon icon="undo" class="button right" v-if="!isRunning" @click="refresh" />
     </div>
 </template>
 
@@ -18,34 +18,32 @@ export default {
     name: 'Interval',
     data() {
         return {
-            duration: 1000,
-            now: new Date(),
             start: null,
             end: null,
-            runnable: true,
-            timeLeft: 1000
+            duration: 10000,
+            timeLeft: 10000,
+            isRunning: true,
         }
     },
     methods: {
         run() {
-            if(this.timeLeft > 0 && this.runnable) {
+            if (this.timeLeft > 0 && this.isRunning) {
                 setTimeout(() => {
-                    this.now = new Date();
-                    this.timeLeft = new Date(this.end - this.now);
 
-                    if (this.timeLeft <= 0) this.runnable = false;
+                    this.timeLeft = new Date(this.end - new Date()).getTime();
+                    if (this.timeLeft <= 0) this.isRunning = false;
 
                     this.run();
                 }, 10);
             }
         },
         play() {
-            this.runnable = true;
+            this.isRunning = true;
             this.computeInterval();
             this.run();
         },
         pause() {
-            this.runnable = false;
+            this.isRunning = false;
         },
         computeInterval() {
             this.start = new Date();
@@ -62,7 +60,7 @@ export default {
                 min = timeLeft.getMinutes(),
                 time = '00:00:000';
 
-            if(timeLeft > 0) {
+            if (this.timeLeft > 0) {
                 // Format number to always have N digits
                 msec = ('000' + msec).substr(-3);
                 sec = ('0' + sec).substr(-2);
@@ -77,9 +75,6 @@ export default {
     computed: {
         finish() {
             return this.timeLeft <= 0;
-        },
-        timeLeftPercent() {
-            return parseInt(this.timeLeft * 100 / new Date(this.end - this.start));
         }
     },
     created() {
