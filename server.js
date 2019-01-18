2260305829e2a2e3d33f05b6e2f14451f77e028a
Nadarajah Mag-Stellon <magstellon@gmail.com>
2019-01-18 15:33:17 +0100

    Add ExpressJS on dependencies and server main file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const serveStatic = require("serve-static");
+const serveStatic = require('serve-static');
 const path = require('path');
-
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+const app = express();
 const port = process.env.PORT || 80;
+
+app.use(serveStatic(path.join(__dirname, 'dist')));
 app.listen(port);
